e0c796b26a139f4d635481f07eee839d7eb76230
mulan <mark.verleg@gmail.com>
2013-08-01 16:51:54 +0200

    settings fixed

--- aqua/settings.py
+++ aqua/settings.py
@@ -9,7 +9,6 @@ STATICFILES_DIRS = (
     '/web/aqua2/static',
 )
 
-<<<<<<< Updated upstream
 from socket import gethostname
 if gethostname() == 'mulan':
     DATABASES = {
@@ -17,16 +16,6 @@ if gethostname() == 'mulan':
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': '.aqua.db',
         }
-=======
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'aqua',
-	'USER': 'aqua',
-	'PASSWORD': 'c8HzbpW5MCCPW8Qm',
-	'HOST': 'localhost',
-	'PORT': 3306,
->>>>>>> Stashed changes
     }
 elif gethostname() == 'bambi':
     DATABASES = {
