ad6b22c495a4079044e7b56b989827e87256c66b
mulan <mark.verleg@gmail.com>
2013-08-02 11:57:27 +0200

    bugfix

--- settings.py
+++ settings.py
@@ -163,7 +163,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'aqua.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
