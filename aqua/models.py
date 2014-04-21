e8f058a1687e9a0d50c75e26bc28deb7bf251134
rafiki <mark.verleg@gmail.com>
2014-04-21 16:51:15 +0200

    finally: names

--- aqua/models.py
+++ aqua/models.py
@@ -13,6 +13,9 @@ class AquaUser(AbstractUser):
     
     objects = UserManager()
     
+    def __repr__(self):
+        return self.__unicode__()
+    
     def __unicode__(self):
         return self.get_full_name() or self.username
     
