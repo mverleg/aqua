40785f34e3cd3468c40efa923c2a6350a035727b
rafiki <mark@rafiki>
2015-08-04 14:23:34 +0200

    readme etc

---
+++ README.rst
@@ -0,0 +1,16 @@
+
+Aqua
+-------------------------------
+
+python manage.py
+
+- runserver: start de testserver
+- calculate_roster: rooster verdelen
+  - eerste argument is ID van rooster (zonder argument geeft overzicht)
+  - -a is de bonus voor meer beschilbaarheid (standaard 18)
+- makemigrations, migrate, syncdb: database updaten (in die volgorde)
+- collectstatic: static files in /static/ zetten (bij serververhuizing)
+- export_backup: zet een backup in /backups/
+- 
+
+
--- distribute/models.py
+++ distribute/models.py
@@ -7,14 +7,14 @@ from settings import AUTH_USER_MODEL
 class UserSlotBase(models.Model):
     user = models.ForeignKey(AUTH_USER_MODEL)
     timeslot = models.ForeignKey(TimeSlot, db_index = True)
-    
+
     def __unicode__(self):
         return '%s @ %s; %s' % (self.user, self.roster.name, self.timeslot.start.strftime(DATETIMEFORMAT))
-    
+
     @property
     def roster(self):
         return self.timeslot.roster
-    
+
     class Meta:
         abstract = True
         unique_together = [['user', 'timeslot', ], ]
@@ -22,11 +22,11 @@ class UserSlotBase(models.Model):
 
 class Availability(UserSlotBase):
     pass
-    
+
 
 class Assignment(UserSlotBase):
-    fortrade = models.IntegerField(default = 0)
+    fortrade = models.IntegerField(default = 0)  #todo: dropdown
     giveto = models.ForeignKey(AUTH_USER_MODEL, blank = True, null = True, related_name = 'assignment_gifts')
     note = models.CharField(max_length = 64, default = '')
-    
+
 
